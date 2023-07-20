0103c821b96552815ca8075a7d7d828edaed0d09
Rohan Sawant <rohansawant@Rohans-MacBook-Air.local>
2023-07-20 11:50:13 +0530

    Ep2 commit

--- App.js
+++ App.js
@@ -1,40 +1,18 @@
-// Manipulate the HTML DOM using Javscript
-// const heading = document.createElement("h1");
-// heading.innerHTML = "Namaste Everyone";
-// const root = document.getElementById("root");
-// root.appendChild(heading);
-
-// Manipulate the HTML DOM using React
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 // Create nested React Elements
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    style: {
-      background: "red",
-    },
-    className: "title",
-  },
-  "heading"
-);
-const heading1 = React.createElement(
-  "h1",
-  {
-    id: "title",
-  },
-  "heading1"
-);
-
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-  },
-  [heading, heading1]
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is Namaste React"),
+    React.createElement("h2", {}, "by Akshay Saini"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 
 // create root using createRoot
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing react element inside root
-root.render(container);
+root.render(parent);
